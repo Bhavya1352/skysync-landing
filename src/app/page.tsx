fc67c9f8f6503ae6bbaf26545e4b290300005e25
bhavya1352 <bhavyamishra698@gmail.com>
2025-11-29 00:49:28 +0530

    Fix: Add use client directive for React hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { CloudIcon, ShieldCheckIcon, BoltIcon, CogIcon, XMarkIcon, PlayIcon, ChatBubbleLeftRightIcon } from '@heroicons/react/24/outline';
 import { useState, useEffect } from 'react';
 
